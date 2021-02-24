59d7e72b096820468072818296a28cc6f80d770a
Caroline Jarman <carolinerosejarman@gmail.com>
2021-02-23 19:12:10 -0700

    adds model classes and data member + generated stuff

---
+++ src/main/java/model/PersonModel.java
@@ -0,0 +1,88 @@
+package model;
+
+public class PersonModel {
+
+  private String personID;
+  private String username;
+  private String firstName;
+  private String lastName;
+  private String gender;
+  private String fatherID;
+  private String motherID;
+  private String spouseID;
+
+  public PersonModel(String personID, String username, String firstName, String lastName, String gender, String fatherID, String motherID, String spouseID) {
+    this.personID=personID;
+    this.username=username;
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.gender=gender;
+    this.fatherID=fatherID;
+    this.motherID=motherID;
+    this.spouseID=spouseID;
+  }
+
+  public String getPersonID() {
+    return personID;
+  }
+
+  public void setPersonID(String personID) {
+    this.personID=personID;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username=username;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName=firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName=lastName;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender=gender;
+  }
+
+  public String getFatherID() {
+    return fatherID;
+  }
+
+  public void setFatherID(String fatherID) {
+    this.fatherID=fatherID;
+  }
+
+  public String getMotherID() {
+    return motherID;
+  }
+
+  public void setMotherID(String motherID) {
+    this.motherID=motherID;
+  }
+
+  public String getSpouseID() {
+    return spouseID;
+  }
+
+  public void setSpouseID(String spouseID) {
+    this.spouseID=spouseID;
+  }
+}
--- src/main/java/model/UserModel.java
+++ src/main/java/model/UserModel.java
@@ -1,4 +1,78 @@
 package model;
 
 public class UserModel {
+
+  private String username;
+  private String personID;
+  private String password;
+  private String email;
+  private String firstName;
+  private String lastName;
+  private String gender;
+
+  public UserModel(String username, String personID, String password, String email, String firstName, String lastName, String gender) {
+    this.username=username;
+    this.personID=personID;
+    this.password=password;
+    this.email=email;
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.gender=gender;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username=username;
+  }
+
+  public String getPersonID() {
+    return personID;
+  }
+
+  public void setPersonID(String personID) {
+    this.personID=personID;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password=password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email=email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName=firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName=lastName;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender=gender;
+  }
 }
